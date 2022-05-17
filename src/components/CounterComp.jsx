dd6f00db5d2f099af7044e9226e5ea7ee7e1f52d
Psykisk <joakim.vanstrom@gmail.com>
2022-05-17 15:16:32 +0200

    counter 2.0 done

--- src/components/CounterComp.jsx
+++ src/components/CounterComp.jsx
@@ -3,10 +3,13 @@ import './styles/Counter.css'
 
 function CounterComp() {
     const [count, setCount] = useState(0);
+    const increment = () => { setCount(count + 1) }
+    const decrement = () => { if(count !== 0){setCount(count - 1) } }
   return (
     <div className='Counter'>
       <h1>You clicked {count} times</h1>
-      <button className='btn-counter' onClick={() => setCount(count + 1)}>Click me</button>
+      <button className='btn-counter' onClick={increment}>Increment</button>
+      <button className='btn-counter' onClick={decrement}>decrement</button>
     </div>
   );
 }
